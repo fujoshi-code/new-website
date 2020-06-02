d4331c7e2111acf97ad603075a2d1c3ef466fc72
fujoshi-code <65970976+fujoshi-code@users.noreply.github.com>
2020-06-02 02:27:26 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,43 @@
+function showEpisode(num) {
+    switch (num) {
+        case 1:
+            window.location.href = 'https://youtube.com/embed/qpUz5NXCpW4?list=PL7hdc7d_X-cQvA1kElALEVD6GnoX7mXar';
+        break;
+        case 2:
+            window.location.href = 'https://youtube.com/embed/K526mjMCDfQ';
+        break;
+        case 3:
+            window.location.href = 'https://youtube.com/embed/2zx68_8jR5g?list=PL7hdc7d_X-cQvA1kElALEVD6GnoX7mXar';
+        break;
+        case 4:
+            window.location.href = 'https://youtube.com/embed/Sejl9v2EQMA?list=PL7hdc7d_X-cQvA1kElALEVD6GnoX7mXar';
+        break;
+        case 5:
+            window.location.href = 'https://youtube.com/embed/67k6SoCniJo?list=PL7hdc7d_X-cQvA1kElALEVD6GnoX7mXar';
+        break;
+        case 6:
+            window.location.href = 'https://youtube.com/embed/2rvZrA-WjZk?list=PL7hdc7d_X-cQvA1kElALEVD6GnoX7mXar';
+        break;
+        case 7:
+            window.location.href = 'https://youtube.com/embed/IxIeS8I5lEY?list=PL7hdc7d_X-cQvA1kElALEVD6GnoX7mXar';
+        break;
+        case 8:
+            window.location.href = 'https://youtube.com/embed/l9PxmPKP6Bs?list=PL7hdc7d_X-cQvA1kElALEVD6GnoX7mXar';
+        break;
+        case 9:
+            window.location.href = 'https://youtube.com/embed/nJJ_mdBFnjo?list=PL7hdc7d_X-cQvA1kElALEVD6GnoX7mXar';
+        break;
+        case 10:
+            window.location.href = 'https://youtube.com/embed/NLZDinOr2VU?list=PL7hdc7d_X-cQvA1kElALEVD6GnoX7mXar';
+        break;
+        case 11:
+            window.location.href = 'https://youtube.com/embed/DwLn8dO-pUA?list=PL7hdc7d_X-cQvA1kElALEVD6GnoX7mXar';
+        break;
+        default:
+            window.location.href = 'https://youtube.com/embed/qpUz5NXCpW4?list=PL7hdc7d_X-cQvA1kElALEVD6GnoX7mXar';
+    }
+}
+
+function submitForm() {
+    alert("You have signed up!")
+}
